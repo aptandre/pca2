7ca749fc537d3744b1ab98665acc988acc1b73b0
André <andre.alves@ccc.ufcg.edu.br>
2025-03-26 16:30:03 -0300

    adding necessary classes, with NO modularization thus far

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+
+from feedback_controller import FeedbackController
 
 app = Flask(__name__)
+
+feedback_controller = FeedbackController()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/feedbacks", methods=['POST'])
+def get_feedback_evaluation():
+    data = request.get_data()
+
+    return feedback_controller.get_evaluation(data)
