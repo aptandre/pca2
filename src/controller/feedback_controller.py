33e3ef611a50de11ed6d412a9239a44d66f318cf
André <andre.alves@ccc.ufcg.edu.br>
2025-03-26 21:25:51 -0300

    E Deus disse: haja modularizacao

---
+++ src/controller/feedback_controller.py
@@ -0,0 +1,38 @@
+from src.service.feature_service import FeatureService
+from src.service.feedback_service import FeedbackService
+from src.service.llm_service import LLMService
+
+class FeedbackController:
+    def __init__(self, db):
+        self.db = db
+        self.feedback_service = FeedbackService(db)
+        self.llm_service = LLMService(db)
+        self.feature_service = FeatureService(db)
+
+    def get_evaluation(self, data):
+
+        feedback_id = data['id']
+        feedback_text = data['feedback']
+
+        self.feedback_service.save_feedback(feedback_id, feedback_text)
+
+        for f in self.feedback_service.get_all_feedbacks():
+            print(f)
+
+
+        #evaluation_result = self.llm_service.evaluate(feedback_id, feedback_text)
+
+        #self.feature_service.save_feature(evaluation_result)
+
+        for f in self.feature_service.get_all_features():
+            print(f)
+
+        #print(evaluation_result)
+
+        return {"ok": True}
+    
+    def get_all_feedbacks(self):
+        return self.feedback_service.get_all_feedbacks()
+    
+    def get_all_features(self):
+        return self.feature_service.get_all_features()
---
+++ src/service/feature_service.py
@@ -0,0 +1,45 @@
+import json
+import MySQLdb
+from src.model.feature import Feature
+
+class FeatureService:
+    def __init__(self, db):
+        self.db = db
+
+    def save_feature(self, evaluation_result):
+        print('O RESULT FOI ESSE: \n\n\n')
+        print(evaluation_result)
+        id = evaluation_result['id']
+        sentiment = evaluation_result['sentiment']
+        features = evaluation_result['features']
+
+        feature = Feature(id, sentiment, features)
+        features = json.dumps(features)
+        try:
+            cur = self.db.connection.cursor()
+            cur.execute(
+                "INSERT INTO feature (id, sentiment, features) VALUES (%s, %s, %s)",
+                (feature.id, feature.sentiment, features)
+            )
+            self.db.connection.commit()
+            cur.close()
+
+            return True
+        
+        except MySQLdb.Error as e:
+            print("Erro ao salvar feature:", e)
+            return False
+        
+    def get_all_features(self):
+        try:
+            cur = self.db.connection.cursor()
+            cur.execute("SELECT id, sentiment, features FROM feature")
+            rows = cur.fetchall()
+            cur.close()
+
+            features = [Feature(row[0], row[1], row[2]) for row in rows]
+            return features
+        
+        except MySQLdb.Error as e:
+            print("Erro ao buscar features:", e)
+            return []
